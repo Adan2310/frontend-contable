1bda52febbf9744c5e398ac33bc7938dbd5dea7e
Adan2310 <150070410+Adan2310@users.noreply.github.com>
2024-12-06 16:59:46 -0600

    fix

--- src/main.js
+++ src/main.js
@@ -6,4 +6,6 @@ import axios from 'axios'
 
 axios.defaults.baseURL='http://localhost:3000/api/'
 
+// axios.defaults.baseURL='backend-contable-audqgae6hxavg9h5.canadacentral-01.azurewebsites.net'
+
 createApp(App).use(store).use(router).mount('#app')
